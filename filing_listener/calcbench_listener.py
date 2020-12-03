05d5d34d897e7189dbad277afe881c4b3b4f08f7
Andrew Kittredge <andrew@calcbench.com>
2020-12-03 09:39:47 -0500

    listener work

--- filing_listener/calcbench_listener.py
+++ filing_listener/calcbench_listener.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-
-from logging import log
+import logging
 import sys
+from pathlib import Path
+
 import calcbench as cb
 
-from pathlib import Path
-import logging
-
-
 connection_string = "Endpoint=sb://calcbench.servicebus.windows.net/;SharedAccessKeyName=public;SharedAccessKey=Cb7VhLR6eJxsurCSPtXQHOJvlkU84CCCx2oB+T/so6Q="
-subscription_name = "not the real subscription"
+subscription_name = "not_real_subscription"
 
 log_handler = logging.StreamHandler(sys.stdout)
 calcbench_logger = logging.getLogger("calcbench")
@@ -20,15 +17,15 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(log_handler)
 
-cb.set_credentials("andrew@calcbench.com", "not my real password")
+# cb.set_credentials("andrew@calcbench.com", "not my real password")
 
 cb.enable_backoff()
 
-output_file_name = r"/home/andrew/push_notification_data.csv"
+output_file_name = Path.joinpath(Path.home(), "push_notification_data.csv")
 
 
 def get_filing_standardized(filing: cb.Filing):
-    if filing["filing_type"] != "annualQuarterlyReport":
+    if not filing.get("standardized_XBRL"):
         return
     accession_id = filing["calcbench_id"]
     filing_data = cb.point_in_time(
@@ -37,8 +34,10 @@ def get_filing_standardized(filing: cb.Filing):
     file_exists = Path(output_file_name).exists()
     logger.info(f"Found {filing_data.shape} for {filing.ticker}")
     if filing_data.shape[0] == 0:
+        msg = f"Found no data for {filing.ticker} {accession_id}"
+        logger.exception(msg)
         # If we didn't find any data there might be something holding up the process on Calcbench's side.  Throw an exception to try again later.
-        raise Exception(f"Found no data for {filing.ticker}")
+        raise Exception(msg)
     filing_data.to_csv(
         output_file_name,
         index=False,
